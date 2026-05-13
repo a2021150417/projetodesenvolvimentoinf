e11a32e5b053004d0da5ecb9ab72559318776ff8
André Barreira <60144317+Andrecas14@users.noreply.github.com>
2026-05-13 01:11:59 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: true,
     proxy: {
-      '/api':{target: 'http://localhost:3001', // O Vite sabe que o teu Node está aqui
+      '/api':{target: 'http://localhost:3001', 
     changeOrigin: true,
     secure: false },
       '/uploads': 'http://localhost:3001'
